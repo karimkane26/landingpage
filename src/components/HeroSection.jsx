540bd96e516400f3a970b5d37bb461ff650ec68e
abdoul <abdoulkarim972012@gmail.com>
2026-05-12 16:54:42 +0000

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import video1 from '../assets/video1.mp4'
+import video2 from '../assets/video2.mp4'
+
+const HeroSection = () => {
+    return(
+        <>
+        <div>HeroSection</div>
+        </>
+    )
+}
+export default HeroSection
